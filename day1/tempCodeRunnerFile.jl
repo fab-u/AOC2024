51de3060ba71485336f43735a06c671d4e205b9f
Fabian Bühlmann <fabianbuehlmann2000@gmail.com>
2024-12-02 15:22:47 +0100

    remove unnessesary files

--- day1/tempCodeRunnerFile.jl
+++
@@ -1,8 +0,0 @@
-A = getindex.(content, 1)  # [1, 3, 5]
-# B = getindex.(content, 2)  # [2, 4, 6]
-
-# A = parse.(Int, A)
-# B = parse.(Int, B)
-
-# sort!(A)
-# sort!(B)
--- day2/tempCodeRunnerFile.jl
+++
@@ -1,3 +0,0 @@
-for g in gradient
-#     println(count(x -> x <= 0, g))
-# end 
